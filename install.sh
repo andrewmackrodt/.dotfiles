ae67ba61e7e2dc86e86053aec8777523a5b6866d
Andrew Mackrodt <andrewmackrodt@gmail.com>
2018-11-28 03:33:10 +0000

    Change PS1 username in Windows

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-grep -q '. ~/.dotfiles/.bashrc' ~/.bashrc 2>/dev/null \
-    || echo "# source dotfiles
-. ~/.dotfiles/.bashrc" >> ~/.bashrc
+DOT_FILES_HOME=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+grep -q ". '$DOT_FILES_HOME/.bashrc'" ~/.bashrc 2>/dev/null \
+    || echo "
+# source dotfiles
+. '$DOT_FILES_HOME/.bashrc'" >> ~/.bashrc
 
 exec bash -l
